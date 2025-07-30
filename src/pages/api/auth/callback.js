e5a5da5218496466332f13998c40cf4498e45241
Ash4Life2024 <ASHWORTHY1KY@GMAIL.COM>
2025-07-29 23:19:47 -0400

    Update callback.js

--- src/pages/api/auth/callback.js
+++ src/pages/api/auth/callback.js
@@ -1,6 +1,10 @@
-<a
-  href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile`}
-  className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
->
-  Sign in with Google
-</a>
+<a href={...}>Sign in with Google</a>
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+export default function HomePage() {
+  const { user } = useUser();
+  return (
+    <main>...Login UI...</main>
+  );
+}
+
